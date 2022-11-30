b6e10888d4952ab5f7eae9df8017cba177be97d1
ankush rawat <ankush.rawat@remotestate.com>
2022-11-30 18:55:58 +0530

    firs-commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-chi/chi"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// http.HandleFunc("/socket", socketHandler)
+	//err := Connect("localhost", "5433", "webSocket", "local", "local", SSLModeDisable)
+	//if err != nil {
+	//	fmt.Printf(" err: %v", err)
+	//}
+	router := chi.NewRouter()
+	router.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+		err := json.NewEncoder(w).Encode("hello world")
+		if err != nil {
+			return
+		}
+	})
+	fmt.Printf("Starting server at port 8081\n")
+	if err := http.ListenAndServe(":8081", router); err != nil {
+		log.Fatal(err)
+	}
+}
